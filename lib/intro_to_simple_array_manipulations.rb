d5e2b427f2aa57bbf99431a9319e64ca48dac653
gregarious-number-3828 <muse.malize@gmail.com>
2019-08-23 21:18:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,13 +1,13 @@
-def using_push(Array, String)
-  Array.push(String)
+def using_push(array, string)
+  array.push(string)
 end
 
-def using_unshift(Array, String)
-  Array.unshift(String)
+def using_unshift(array, atring)
+  array.unshift(string)
 end
 
-def using_pop(Array)
-  Array.pop
+def using_pop(array)
+  array.pop
 end
 
 def pop_with_args(array)
@@ -17,5 +17,6 @@ end
 def using_shift(array)
   array.shift
 end
+
 
 
